37ed4e920b6f67f8b2e9b20561ce5fa56f957f3b
AlgoWebBot <shimulzahan636@gmail.com>
2023-12-29 04:09:08 +0600

    final push backend

--- src/api/taskGetApi.js
+++ src/api/taskGetApi.js
@@ -2,8 +2,12 @@ const express = require('express');
 const Task = require('../model/task/task');
 
 const getTask = async (req, res, next) => {
-    const result = await Task.find();
+    console.log('hit');
+    const email = req.query.email;
+    console.log(email);
+    const result = await Task.find({email: email});
     res.status(200).json(result);
+
 }
 
 module.exports = getTask;
